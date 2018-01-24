f02d35813a1c020b38b9a187b879036c9fc4f4a1
Yuhan Lin <yuhanlin1105@gmail.com>
2018-01-23 22:47:21 -0500

    init server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const config = require('./config');
+const contacts =require('./contacts');
 
 const app = express();
+
+app.use(express.static('public'));
+app.use(cors());
+
 
 app.get('/',(req,res)=>{
     const help=`
@@ -21,8 +28,39 @@ app.get('/',(req,res)=>{
     </pre>
     `
     res.send(help);
+});
+
+app.use((req,res,next)=>{
+    const token = req.get('Authorization');
+
+    if(token){
+        req.token=token;
+    }else{
+        res.status('403').send({
+            error:'Please provide an Authorization header to identify yourself'
+        });
+    }
+});
+
+app.get('/contacts',(req,res)=>{
+    res.send(Contacts.get(req.token));
 })
 
+app.delete('/contacts/:id',(req,res)=>{
+    res.send(contacts.remove(req.token, req.params.id));
+})
+
+app.post('/contacts',bodyParser.json(), (req,res)=>{
+    const { name, email} = req.body;
+    if(name&&email){
+        res.send(contacts.add(req.token, req.body));
+    }else{
+        res.status(403).send({
+            error:'Please provide both a name and email address'
+        });
+    }
+
+})
 app.listen(config.port,()=>{
     console.log(`Server listening on port ${config.port}, Ctrl+C to stop`);
 })
